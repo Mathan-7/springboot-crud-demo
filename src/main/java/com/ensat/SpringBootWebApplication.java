88e0256ca21ed77499a653b6457a99af65b8073c
Mathan-7 <64543633+Mathan-7@users.noreply.github.com>
2020-04-30 16:32:16 +0530

    Update SpringBootWebApplication.java

--- src/main/java/com/ensat/SpringBootWebApplication.java
+++ src/main/java/com/ensat/SpringBootWebApplication.java
@@ -2,12 +2,28 @@ package com.ensat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
-public class SpringBootWebApplication {
+public class SpringBootWebApplication extends SpringBootServletInitializer {
+{
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootWebApplication.class, args);
     }
+    
+    @Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(CustomerServiceApplication.class);
+		
+	}
 
 }
